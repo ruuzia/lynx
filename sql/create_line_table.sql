a9284876fbdbdc58663320db3acce8aaca043a1d
Rustum Zia <ziarustum@gmail.com>
2025-03-04 21:09:10 -0500

    Add columns to create_line_table.sql

--- sql/create_line_table.sql
+++ sql/create_line_table.sql
@@ -1,9 +1,14 @@
 CREATE TABLE line_data (
     id int NOT NULL AUTO_INCREMENT,
     user_id int,
+    line_set_id int,
     line_number int,
+    location VARCHAR(16),
     cue TEXT(65000),
     line TEXT(65000),
+    notes TEXT(65000),
+    starred boolean,
     PRIMARY KEY(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (line_set_id) REFERENCES line_sets(id)
 );
