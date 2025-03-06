f24adcd5767f43f0621000a4ca03fbde5ce00461
Rustum Zia <ziarustum@gmail.com>
2025-03-05 20:26:12 -0500

    [colorscheme.ts]

---
+++ web/static/ts/colorscheme.ts
@@ -0,0 +1,20 @@
+const setting = localStorage.getItem("colorscheme");
+if (setting !== null) {
+    document.body.classList.add(setting);
+} else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    document.body.classList.add("dark");
+} else {
+    document.body.classList.add("light");
+}
+
+function toggleTheme() {
+    if (document.body.classList.contains("dark")) {
+        document.body.classList.remove("dark");
+        document.body.classList.add("light");
+        localStorage.setItem("colorscheme", "light");
+    } else {
+        document.body.classList.remove("light");
+        document.body.classList.add("dark");
+        localStorage.setItem("colorscheme", "dark");
+    }
+}
