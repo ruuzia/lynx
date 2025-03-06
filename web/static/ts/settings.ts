bb67353c99c771c977331de52997e27456064155
Rustum Zia <ziarustum@gmail.com>
2025-03-06 15:31:51 -0500

    Add settings.ts

---
+++ web/static/ts/settings.ts
@@ -0,0 +1,46 @@
+export const settings = {
+    reviewMethod: null,
+};
+
+const reviewStrategies = [
+    {
+        id: "in_order",
+        title: "In order",
+        description: "Review lines in order"
+    },
+    {
+        id: "random",
+        title: "Random order",
+        description: "Review lines in order"
+    },
+    {
+        id: "cues",
+        title: "Cues from lines",
+        description: "Difficult: recall cues from lines"
+    },
+    {
+        id: "no_cues",
+        title: "No cues",
+        description: "Advanced: recall lines onlyl based on order"
+    },
+];
+
+function init() {
+    const settingsList = document.getElementById("settings-strategy-list");
+    if (settingsList === null) {
+        throw new Error("Missing #settings-strategy-list");
+    }
+
+    let s = ``;
+    for (const { id, title, description } of reviewStrategies) {
+        s += `
+<a class="button-thick"
+   href="/#reviewer">
+${title}
+</a>
+        `;
+    }
+    settingsList.innerHTML = s;
+}
+
+init();
