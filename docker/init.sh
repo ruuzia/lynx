c54527cd0ef12752c9eb6997c06f5cb75fdcfd85
Rustum Zia <ziarustum@gmail.com>
2025-03-14 21:40:52 -0400

    [init.sh]

--- docker/init.sh
+++ docker/init.sh
@@ -6,14 +6,14 @@ mkdir -p /run/secrets
 
 if [-v LYNX_DB_PASSWORD ]; then
   echo $LYNX_DP_PASSWORD > /run/secrets/db_password
-fi;
+fi
 
 if [-v LYNX_EMAIL_PASSWORD ]; then
   echo $LYNX_EMAIL_PASSWORD > /run/secrets/email_password
-fi;
+fi
 
 if [-v LYNX_JWT_SECRET ]; then
   echo $LYNX_JWT_SECRET > /run/secrets/jwt_secret
-fi;
+fi
 
 /feline
